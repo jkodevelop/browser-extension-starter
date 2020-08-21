2f4668fc837db888fbdefcbd335114dbeb3c7ee3
jasonko-home <jasonko@sb2.ca>
2020-08-21 01:04:10 -0400

    step 4: setting up gulp to process sass 1. npm i gulp-sass --save-dev 2. setting up a new task sass(), process style.scss with gulp-sass then move to publish/css/style.css 3. index.html updated to load css/style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
+const gulpSass = require('gulp-sass'); 
 
-function copyStatic() {
-    return src('./src/static/index.html', { allowEmpty: true }) 
-        .pipe(dest('./publish'));
+function sass(){
+  return src('./src/css/style.scss', { allowEmpty: true })
+      .pipe(gulpSass())     
+      .pipe(dest('./publish/css'));
 }
 
-const build = series(copyStatic);
+function copyStatic() {
+  return src('./src/static/index.html', { allowEmpty: true }) 
+      .pipe(dest('./publish'));
+}
+
+
+const build = series(parallel(sass, copyStatic));
+
+
+// this allows you to just run sass in command line
+exports.sass = sass; // $ gulp sass
 
 exports.build = build;
 
