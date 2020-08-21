ebf53a4f014ca3a5038d9a9b2b8f9e0b1912f1d6
jasonko-home <jasonko@sb2.ca>
2020-08-21 00:07:24 -0400

    step 2: set up your first gulp default task example: gulpfile.js 1. terminal $: gulp

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 http://zetcode.com/gulp/getting-started/
-
-
-npm i --global gulp-cli
-npm i gulp --save-dev
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,7 @@
-// this is where gulp checks its tasks
+function defaultTask(cb) {
+  // place code for your default task here
+  console.log('Hello Gulp');
+  cb(); // tells gulp you are done
+}
+
+exports.default = defaultTask
