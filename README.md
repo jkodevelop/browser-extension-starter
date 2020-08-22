f0f1e451957b4ef04ba5c4a65b7b47e003e403e4
jasonko-home <jasonko@sb2.ca>
2020-08-22 00:35:39 -0400

    step 5: adding javascript processing task, introducing "gulp-rename" 1. npm install gulp-rename --save-dev 2. setting up js task and renaming the output file from src/js/index.js to publish/js/script.min.js 3. adding scripts into index.html

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ http://zetcode.com/gulp/getting-started/
 https://gulpjs.com/docs/en/getting-started/creating-tasks
 https://www.freecodecamp.org/news/super-simple-gulp-tutorial-for-beginners-45141974bfe8/
 https://css-tricks.com/gulp-for-beginners/
+https://gulpjs.com/docs/en/getting-started/watching-files
+
+https://gist.github.com/stsvilik/46bf34f76910fcf515ea00650134190f
+https://www.browsersync.io/docs/gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const { src, dest, series, parallel } = require('gulp');
 const gulpSass = require('gulp-sass'); 
 const cssnano = require('gulp-cssnano'); 
+const gulpRename = require('gulp-rename');
+
+function js(){
+  return src('./src/js/index.js', { allowEmpty: true })
+      .pipe(gulpRename({
+        basename: 'script',
+        suffix: '.min'
+      })) // rename javascript to script.min.js
+      .pipe(dest('./publish/js')); // then copy to this location
+}
 
 function sass(){
   return src('./src/css/style.scss', { allowEmpty: true })
@@ -15,11 +25,12 @@ function copyStatic() {
 }
 
 
-const build = series(parallel(sass, copyStatic));
+const build = series(parallel(sass, js, copyStatic));
 
 
 // this allows you to just run sass in command line
 exports.sass = sass; // $ gulp sass
+exports.js = js; // $ gulp js
 
 exports.build = build;
 
